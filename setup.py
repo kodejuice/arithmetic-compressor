6e68f2d16bdfa1f52379aa8ee79c9984bee0ce27
kodejuice <kodejuice@gmail.com>
2023-01-27 09:21:32 +0100

    package setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+VERSION = '0.1'
+setup(
+    name='arithmetic-compressor',
+    version=VERSION,
+    author='Biereagu Sochima',
+    author_email='<sochima.eb@gmail.com>',
+    description='An implementation of the Arithmetic Coding algorithm in Python, along with advanced models like PPM (Prediction by Partial Matching), Context Mixing and Simple Adaptive models',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/kodejuice/arithmetic_compressor',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    keywords=['arithmetic', 'coding', 'ppm', 'encoding',
+              'prediction', 'context mixing', 'adaptive models'],
+    python_requires='>=3.0',
+)

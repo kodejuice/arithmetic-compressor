c35d1a5335b165776b88e5c1110aa14b1cf15edd
kodejuice <kodejuice@gmail.com>
2023-01-29 20:46:00 +0100

    ver 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
   long_description = fh.read()
 
-VERSION = '0.1'
+VERSION = '0.2'
 setup(
     name='arithmetic_compressor',
     version=VERSION,
